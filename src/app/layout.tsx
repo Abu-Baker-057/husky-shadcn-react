2916e43a7687a9abb004a0982c34b7ca511c30ec
Abu Baker Shad <abubakershad057@gmail.com>
2024-10-18 18:26:28 +0500

    shadcn, react basics and husky integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,9 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,9 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={inter.className}>
+        <div className="bg-slate-200 h-12 flex gap-5 px-5 font-bold items-center">
+          <Link href={"/"}>Home</Link>
+          <Link href="/reactAdvanced">React Advanced</Link>
+          <Link href="/reactAdvanced/reactStates">React States</Link>
+          <Link href="/reactAdvanced/reactLifeCycle">React Lifecycle</Link>
+        </div>
         {children}
       </body>
     </html>
